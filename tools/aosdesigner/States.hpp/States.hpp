284803c59b42c5e54adb57a0a6ed400b4509b0eb
klaim <klaim@localhost>
2011-07-05 09:18:28 +0200

    AOSDesigner : adding files for the application state machine.

---
+++ tools/aosdesigner/States.hpp/States.hpp
@@ -0,0 +1,24 @@
+#ifndef __AOSD__STATES_HPP__
+#define __AOSD__STATES_HPP__
+#pragma once
+
+namespace aosd
+{
+namespace state
+{
+
+	/** TODO :
+		Add here the state transition table for the major states.
+		
+		States :
+		 - Welcome (?) : let the user choose what to do
+			-> Opening project
+			-> Creating project
+		 - Project Edition : let the user edit the whole project
+			-> Sequence edition?
+	*/
+	
+
+}
+}
+#endif
