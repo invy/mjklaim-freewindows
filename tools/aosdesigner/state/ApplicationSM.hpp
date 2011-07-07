3eeed5ab7029704d5198581c46397ba5a468779a
klaim <klaim@localhost>
2011-07-07 22:12:16 +0200

    AOSD : added the class for the application state machine.

---
+++ tools/aosdesigner/state/ApplicationSM.hpp
@@ -0,0 +1,25 @@
+#ifndef HGUARD_AOSD_STATE_APPLICATIONSM_HPP__
+#define HGUARD_AOSD_STATE_APPLICATIONSM_HPP__
+#pragma once
+
+
+
+namespace aosd
+{
+namespace state
+{
+	
+
+	class ApplicationSM
+	{
+	public:
+		
+	private:
+
+	};
+
+}
+}
+
+
+#endif
