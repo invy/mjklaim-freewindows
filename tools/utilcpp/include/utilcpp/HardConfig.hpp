a02661ef18871cb86ad053d6cd60eebae7ffd7e8
Klaim <klaim@artofsequence.org>
2011-08-17 20:32:31 +0200

    UTILCPP : made sure that the behaviour of macros can be overloaded.

--- tools/utilcpp/include/utilcpp/HardConfig.hpp
+++ tools/utilcpp/include/utilcpp/HardConfig.hpp
@@ -3,22 +3,44 @@
 #pragma once
 
 
-#ifdef _DEBUG 
-	#define UTILCPP_DEBUG_LOG_ON 1
-#else
-	#define UTILCPP_DEBUG_LOG_ON 0
+#ifndef UTILCPP_DEBUG_LOG_ON 
+	#ifdef _DEBUG 
+		#define UTILCPP_DEBUG_LOG_ON 1
+	#else
+		#define UTILCPP_DEBUG_LOG_ON 0
+	#endif
 #endif
 
-#ifdef _DEBUG
-	#define UTILCPP_ASSERTIONS_ON 1
-	#define UTILCPP_ASSERTIONS_LOG_ON 1
-	#define UTILCPP_ASSERTIONS_DEBUGBREAK_ON 1
-	#define UTILCPP_ASSERTIONS_CRASH_ON 1
-#else
-	#define UTILCPP_ASSERTIONS_ON 0
-	#define UTILCPP_ASSERTIONS_LOG_ON 0
-	#define UTILCPP_ASSERTIONS_DEBUGBREAK_ON 0
-	#define UTILCPP_ASSERTIONS_CRASH_ON 0
+#ifndef UTILCPP_ASSERTIONS_ON 
+	#ifdef _DEBUG 
+		#define UTILCPP_ASSERTIONS_ON 1
+	#else
+		#define UTILCPP_ASSERTIONS_ON 0
+	#endif
+#endif
+
+#ifndef UTILCPP_ASSERTIONS_LOG_ON 
+	#ifdef _DEBUG 
+		#define UTILCPP_ASSERTIONS_LOG_ON 1
+	#else
+		#define UTILCPP_ASSERTIONS_LOG_ON 0
+	#endif
+#endif
+
+#ifndef UTILCPP_ASSERTIONS_DEBUGBREAK_ON 
+	#ifdef _DEBUG 
+		#define UTILCPP_ASSERTIONS_DEBUGBREAK_ON 1
+	#else
+		#define UTILCPP_ASSERTIONS_DEBUGBREAK_ON 0
+	#endif
+#endif
+
+#ifndef UTILCPP_ASSERTIONS_CRASH_ON 
+	#ifdef _DEBUG 
+		#define UTILCPP_ASSERTIONS_CRASH_ON 1
+	#else
+		#define UTILCPP_ASSERTIONS_CRASH_ON 0
+	#endif
 #endif
 
 
