3186240ee8e6658964d250f8babd7a0ebbe67adb
Klaim <Klaim@KlaimSystem>
2011-11-09 17:44:06 +0100

    AOSD : added better validation of edission session ids

--- tools/aosdesigner/core/EditionSessionId.hpp
+++ tools/aosdesigner/core/EditionSessionId.hpp
@@ -2,12 +2,18 @@
 #define HGUARD_AOSD_CORE_EDITIONSESSIONID_HPP__
 #pragma once
 
+#include <string>
 
 namespace aosd
 {
 namespace core
 {
 	typedef std::string EditionSessionId;
+
+	extern const EditionSessionId EditionSessionId_INVALID;
+
+	bool is_valid( const EditionSessionId& session_id );
+
 }
 }
 
