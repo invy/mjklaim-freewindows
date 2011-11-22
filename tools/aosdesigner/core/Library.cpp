0a45f2fffc8577174ae56c0055bf95d8b92aded5
klaim <klaim@localhost>
2011-11-22 19:47:22 +0100

    AOSD : not sure yet about the exact role of Library and Resource...

--- tools/aosdesigner/core/Library.cpp
+++ tools/aosdesigner/core/Library.cpp
@@ -1,12 +1,35 @@
 #include "Library.hpp"
 
+#include "core/Resource.hpp"
 
+#include "aosl/library.hpp"
 
 namespace aosd
 {
 namespace core
 {
 
+
+
+	Library::Library()
+	{
+
+	}
+
+	Library::~Library()
+	{
+
+	}
+
+	void Library::update( const aosl::Library& library_info )
+	{
+
+	}
+
+	void Library::import( const Library& library )
+	{
+
+	}
 
 }
 }
--- tools/aosdesigner/core/Library.hpp
+++ tools/aosdesigner/core/Library.hpp
@@ -2,19 +2,44 @@
 #define HGUARD_AOSD_CORE_LIBRARY_HPP__
 #pragma once
 
+#include <vector>
+#include <algorithm>
+
+
+namespace aosl
+{
+	class Library;
+}
 
 namespace aosd
 {
 namespace core
 {
+	class Resource;
+
 	class Library
 	{
 	public:
-		
-		
 
+		Library();
+		~Library();
+
+		/** Update the content of the library. **/
+		void update( const aosl::Library& library_info );
+				
+		/** Import resources from another library. **/
+		void import( const Library& library );
+
+		/** Read-only walk through all resources in this Library. **/
+		template< class Func >
+		void for_each_resource( Func func )
+		{
+			std::for_each( m_resources.begin(), m_resources.end(), [&]( const Resource& resource ){ func(resource); } );
+		}
+		
 	private:
 
+		std::vector< Resource > m_resources;
 
 	};
 
